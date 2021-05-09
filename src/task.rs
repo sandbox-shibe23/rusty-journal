b8ac036f7aba88decaf5ab1c8959a590a770dbb1
shibe23 <shibe23.info@gmail.com>
2021-05-09 23:34:13 +0900

    feat: modify tasks

---
+++ src/task.rs
@@ -0,0 +1,33 @@
+use chrono::{serde::ts_seconds, DateTIme, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task {
+    pub text: String,
+    
+    #[serde(with = "ts_seconds")]
+    pub created_at: DateTime<Utc>,
+}
+
+impl Task {
+    pub fn new(text: String) -> Task {
+        let created_at: Datetime<Utc> = Utc::now();
+        Task {text, created_at}
+    }
+}
+
+use std::io::Result;
+use std::path::PathBuf;
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    
+}
