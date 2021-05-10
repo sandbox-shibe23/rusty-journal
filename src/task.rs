5162793ab0f263687f7e8ad9792ffa90337a0edb
shibe23 <shibe23.info@gmail.com>
2021-05-10 23:41:18 +0900

    feat: implement add_task

--- src/task.rs
+++ src/task.rs
@@ -17,11 +17,32 @@ impl Task {
     }
 }
 
+
 use std::io::Result;
 use std::path::PathBuf;
 
+use std::fs::OpenOptions;
+use std::io::{BufReader, Result, Seek, SeekFrom};
+
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
     
+    let mut tasks: Vec<Tasks> = match serde_json::from_read(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?
+    };
+    
+    file.seek(SeekFrom::Start(0))?;
+    
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+    
+    Ok(())
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
