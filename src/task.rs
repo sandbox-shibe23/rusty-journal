9c3b818f17cf4948ff8c4eac0289f6acc2294eb3
shibe23 <shibe23.info@gmail.com>
2021-05-11 00:33:11 +0900

    feat: add complete_task

--- src/task.rs
+++ src/task.rs
@@ -31,24 +31,41 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
         .create(true)
         .open(journal_path)?;
     
-    let mut tasks: Vec<Tasks> = match serde_json::from_read(file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?
-    };
-    
-    file.seek(SeekFrom::Start(0))?;
-    
+    let mut tasks = collect_tasks(&file)?;
     tasks.push(task);
     serde_json::to_writer(file, &tasks)?;
-    
     Ok(())
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+   let file = OpenOptions::new()
+       .read(true)
+       .write(true)
+       .open(journal_path)?;
+
+    let mut tasks = collect_tasks(&file)?;
     
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind:InvalidInput, "Invalid Task ID"));
+    }
+    tasks.remove(task_position - 1);
+    
+    file.set_ken(0)?;
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
 }
 
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
     
+}
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?;
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?;
+    Ok(tasks)
 }
